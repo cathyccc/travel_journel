82297a0b7164130c2c23047b995dfc67dd740eed
cathyccc <cathyky.chan@gmail.com>
2025-12-03 15:41:14 -0500

    added array of imported data, adjusted entry component to be dynamic, fixed styling on overflow of text and header background being cut off

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Header from './components/Header'
 import Entry from './components/Entry'
+import data from './data'
 import './App.css'
 
 function App() {
+  const entries = data.map(entry => {
+      return (
+        <Entry 
+            img={entry.img}
+            title={entry.title}
+            country={entry.country}
+            googleMapsLink={entry.googleMapsLink}
+            dates={entry.dates}
+            text={entry.text}
+        />
+      )
+  })
+
   return (
     <>
       <Header/>
-      <Entry/>
+      {entries}
     </>
   )
 }
